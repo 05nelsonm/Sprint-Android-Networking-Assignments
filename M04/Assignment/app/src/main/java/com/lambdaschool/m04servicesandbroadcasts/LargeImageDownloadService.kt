56030204794792f40abff537e2952f53122c2c61
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-05 19:56:01 -0400

    created service for downloading an image

--- M04/Assignment/app/src/main/java/com/lambdaschool/m04servicesandbroadcasts/LargeImageDownloadService.kt
+++ M04/Assignment/app/src/main/java/com/lambdaschool/m04servicesandbroadcasts/LargeImageDownloadService.kt
@@ -1,23 +1,19 @@
-package com.lambdaschool.serviceimagedownloader
+package com.lambdaschool.m04servicesandbroadcasts
 
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import java.lang.UnsupportedOperationException
 
-// TODO: S04M04-1 create new service
 class LargeImageDownloadService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.i("LargeImageDownload", " - started")
 
-        // TODO: S04M04-2 Override onStartCommand
         Thread(Runnable() {
-            // TODO: S04M04-3 Add network call
             val bitmap = NetworkAdapter.getBitmapFromUrl("https://i.imgur.com/HaSmgGn.jpg")
-
             val intent = Intent(FILE_DOWNLOADED_ACTION).apply {
                 putExtra(DOWNLOADED_IMAGE, bitmap)
             }
@@ -26,7 +22,7 @@ class LargeImageDownloadService : Service() {
             stopSelf()
         }).start()
 
-        return super.onStartCommand(intent, flags, startId)
+    return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onCreate() {
@@ -34,20 +30,17 @@ class LargeImageDownloadService : Service() {
         Log.i("LargeImageDownload", " - created")
     }
 
+    override fun onBind(p0: Intent?): IBinder? {
+        throw UnsupportedOperationException()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.i("LargeImageDownload", " - destroyed")
     }
 
-    override fun onBind(p0: Intent?): IBinder? {
-        throw UnsupportedOperationException()
-
-    }
-
     companion object {
-        const val FILE_DOWNLOADED_ACTION = "com.lambdaschool.serviceimagedownloader.FILE_DOWNLOADED"
+        const val FILE_DOWNLOADED_ACTION = "com.lambdaschool.com.lambdaschool.m04servicesandbroadcasts.FILE_DOWNLOADED"
         const val DOWNLOADED_IMAGE = "downloadedImage"
     }
 }
-
-
